fe27795b1c8d6a44354f54e00c75e3f674a40568
Rishabh Kumrawat <rishabhkumrawat40@gmail.com>
2023-12-09 09:14:45 +0530

    Server set up

---
+++ Server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = 8000 || process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const server = app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
