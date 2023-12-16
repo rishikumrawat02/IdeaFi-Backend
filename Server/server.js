717859837447f059d4a8e6249c4478955efabb4d
Rishabh Kumrawat <97178821+rishikumrawat02@users.noreply.github.com>
2023-12-16 08:06:10 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const PORT = 8000 || process.env.PORT;
+const PORT = process.env.PORT  || 8000;
 
 app.use(cors());
 
